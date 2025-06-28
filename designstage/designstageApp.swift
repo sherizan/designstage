9d5c55bbe4038aca27e77afd77022e20d211fd86
Sherizan <sherizan@gmail.com>
2025-06-28 22:05:00 +0400

    Initial Commit

---
+++ designstage/designstageApp.swift
@@ -0,0 +1,17 @@
+//
+//  designstageApp.swift
+//  designstage
+//
+//  Created by Sherizan Sheikh on 28/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct designstageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ designstageTests/designstageTests.swift
@@ -0,0 +1,17 @@
+//
+//  designstageTests.swift
+//  designstageTests
+//
+//  Created by Sherizan Sheikh on 28/06/2025.
+//
+
+import Testing
+@testable import designstage
+
+struct designstageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
